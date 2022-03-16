db2ad47226e09e7f3758cdbdda317dfcee026c60
jerlyssonm <jerlyssonm@gmail.com>
2022-03-16 00:25:19 -0300

    fix: refactor

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import express from 'express';
-import {
-  verifyUserExist,
-  alreadyExistUser,
-  alreadyExistNote,
-  userList,
-} from './middlewares.js';
-import User from './userModel.js';
-import Notes from './noteModel.js';
+import {v4} from 'uuid';
 
 const app = express();
+const userList = []
 app.use(express.json());
+
+function verifyUserExist(req, res, next) {
+  const data = req.body;
+  const { cpf } = req.params;
+  const user = userList.find((obj) => obj.cpf === cpf);
+  if (!user) {
+    res.status(404).json({ error: 'user is not registered' });
+    return;
+  }
+  if (data.cpf && user.cpf !== data.cpf) {
+    res.status(404).json({ message: 'CPF cannot be changed' });
+    return;
+  }
+  return next();
+}
+
+function alreadyExistUser(req, res, next) {
+  const { cpf } = req.body;
+  const user = userList.find((obj) => obj.cpf === cpf);
+  if (user) {
+    res.status(422).json({ error: 'user already exists' });
+    return;
+  }
+  return next();
+}
+function alreadyExistNote(req, res, next) {
+  const { cpf, id } = req.params;
+  const user = userList.find((obj) => obj.cpf === cpf);
+  const note = user.notes.find((obj) => obj.id === id);
+  if (!note) {
+    res.status(404).json({ error: 'note is not registered' });
+    return;
+  }
+  return next();
+}
 
 app.post('/users', alreadyExistUser, (req, res) => {
   const { name, cpf } = req.body;
@@ -25,13 +54,13 @@ app.get('/users', (_, res) => {
 app.patch('/users/:cpf', verifyUserExist, (req, res) => {
   const data = req.body;
   const { cpf } = req.params;
-  const user = userList.filter((obj) => obj.cpf === cpf);
+  const user = userList.find((obj) => obj.cpf === cpf);
 
-  user[0].updateUse(data.name);
+  user.updateUse(data.name);
 
   res.json({
     message: 'User is updated',
-    user: user[0],
+    user: user,
   });
 });
 
@@ -45,19 +74,19 @@ app.post('/users/:cpf/notes', verifyUserExist, (req, res) => {
   const { title, content } = req.body;
   const { cpf } = req.params;
 
-  const user = userList.filter((obj) => obj.cpf === cpf);
+  const user = userList.find((obj) => obj.cpf === cpf);
   const note = new Notes(title, content);
 
-  user[0].setNotes(note);
+  user.setNotes(note);
   res
     .status(201)
-    .json({ message: `${note.title} was added into ${user[0].name}` });
+    .json({ message: `${note.title} was added into ${user.name}` });
 });
 
 app.get('/users/:cpf/notes', verifyUserExist, (req, res) => {
   const newCpf = req.params.cpf;
-  const user = userList.filter((obj) => obj.cpf === newCpf);
-  res.json(user[0].notes);
+  const user = userList.find((obj) => obj.cpf === newCpf);
+  res.json(user.notes);
 });
 
 app.patch(
@@ -68,10 +97,10 @@ app.patch(
     const { cpf, id } = req.params;
     const { title, content } = req.body;
 
-    const user = userList.filter((obj) => obj.cpf === cpf);
-    const note = user[0].notes.filter((obj) => obj.id === id);
-    note[0].updateNote(title, content);
-    res.json(note[0]);
+    const user = userList.find((obj) => obj.cpf === cpf);
+    const note = user.notes.find((obj) => obj.id === id);
+    note.updateNote(title, content);
+    res.json(note);
   }
 );
 
@@ -81,10 +110,41 @@ app.delete(
   alreadyExistNote,
   (req, res) => {
     const { cpf, id } = req.params;
-    const user = userList.filter((obj) => obj.cpf === cpf);
-    user[0].notes.pop(id);
+    const user = userList.find((obj) => obj.cpf === cpf);
+    user.notes.pop(id);
     res.status(204).json();
   }
 );
+
+class User {
+  constructor(name, cpf) {
+    this.id = v4();
+    this.name = name;
+    this.cpf = cpf;
+    this.notes = [];
+  }
+
+  setNotes(note) {
+    this.notes.push(note);
+  }
+
+  updateUse(name) {
+    this.name = name;
+  }
+}
+class Notes {
+  constructor(title, content) {
+    this.id = v4();
+    this.title = title;
+    this.content = content;
+    this.created_at = new Date();
+  }
+
+  updateNote(title, content) {
+    this.title = title;
+    this.content = content;
+    this.updated_at = new Date();
+  }
+}
 
 app.listen(3000);
